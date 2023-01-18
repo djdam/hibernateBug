6c80442b9c72f66f760fe8fd5426f5a69ede8e55
Christoph Watzl <christoph.watzl@risc-software.at>
2023-01-18 16:33:04 +0100

    Polish

--- src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,8 @@
 package org.hibernate.bugs;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-
 import org.hibernate.bugs.model.Child;
 import org.hibernate.bugs.model.Grandchild;
 import org.hibernate.bugs.model.Parent;
@@ -11,41 +11,50 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.function.Consumer;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
  */
 public class JPAUnitTestCase {
 
-	private EntityManagerFactory entityManagerFactory;
+    private EntityManagerFactory entityManagerFactory;
 
-	@Before
-	public void init() {
-		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-	}
+    @Before
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("templatePU");
+    }
 
-	@After
-	public void destroy() {
-		entityManagerFactory.close();
-	}
+    @After
+    public void destroy() {
+        entityManagerFactory.close();
+    }
 
-	@Test
-	public void hhh16007Test() throws Exception {
-		var entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-
-		var parent = new Parent("123", new Child(List.of(new Grandchild("xyz"))));
-		entityManager.persist(parent);
-		entityManager.flush();
-		entityManager.clear();
-
-		entityManager.remove(entityManager.getReference(Parent.class, "123"));
-		entityManager.flush();
+    @Test
+    public void hhh16007Test() throws Exception {
+        doInTransaction(entityManager -> {
+            var parent = new Parent("123", new Child(List.of(new Grandchild("xyz"))));
+            entityManager.persist(parent);
+        });
 
 
-		entityManager.getTransaction().commit();
-		entityManager.close();
-	}
+        assertThatCode(() ->
+                doInTransaction(entityManager ->
+                        entityManager.remove(entityManager.getReference(Parent.class, "123"))
+                )
+        ).doesNotThrowAnyException();
+    }
+
+    private void doInTransaction(Consumer<EntityManager> action) {
+        var entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            action.accept(entityManager);
+            entityManager.getTransaction().commit();
+        } finally {
+            entityManager.close();
+        }
+    }
 }
